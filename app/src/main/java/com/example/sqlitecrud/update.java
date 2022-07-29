78ec98f9eb4ef70de37c6e981120def295ca9e77
Mahfuj Hassan <mahfujhassan@mahfujhassan14.onmicrosoft.com>
2022-07-30 03:34:59 +0600

    Update page

--- app/src/main/java/com/example/sqlitecrud/update.java
+++ app/src/main/java/com/example/sqlitecrud/update.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
@@ -15,7 +16,7 @@ import com.google.android.material.textfield.TextInputLayout;
  */
 public class update extends AppCompatActivity {
 
-    TextInputLayout sys, dias, hr,cmnt;
+    EditText sys, dias, hr,cmnt;
 
     Button sbmt;
 
@@ -27,10 +28,10 @@ public class update extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
-        sys = (TextInputLayout) findViewById(R.id.upSYs);
-        dias = (TextInputLayout) findViewById(R.id.updias);
-        hr = (TextInputLayout) findViewById(R.id.uphrtRate);
-        cmnt= (TextInputLayout) findViewById(R.id.upcomment) ;
+        sys =  findViewById(R.id.upSYs);
+        dias = findViewById(R.id.updias);
+        hr =  findViewById(R.id.uphrtRate);
+        cmnt=  findViewById(R.id.upcomment) ;
         sbmt = (Button) findViewById(R.id.sbmt_up);
 
         Bundle bundle = getIntent().getBundleExtra("userdata");
@@ -40,15 +41,15 @@ public class update extends AppCompatActivity {
         String s3 = bundle.getString("pulse");
         String s4 = bundle.getString("comment");
 
-        sys.getEditText().setText(s1);
-        dias.getEditText().setText(s2);
-        hr.getEditText().setText(s3);
-        cmnt.getEditText().setText(s4);
+        sys.setText(s1);
+        dias.setText(s2);
+        hr.setText(s3);
+        cmnt.setText(s4);
         sbmt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                editAction(id,sys.getEditText().getText().toString(),dias.getEditText().getText().toString(),hr.getEditText().getText().toString(),cmnt.getEditText().getText().toString());
+                editAction(id,sys.getText().toString(),dias.getText().toString(),hr.getText().toString(),cmnt.getText().toString());
             }
         });
     }
@@ -66,10 +67,10 @@ public class update extends AppCompatActivity {
         if(recedit!=-1)
         {
             Toast.makeText(getApplicationContext(),"Update Successfully",Toast.LENGTH_SHORT).show();
-            sys.getEditText().setText("");
-            dias.getEditText().setText("");
-            cmnt.getEditText().setText("");
-            hr.getEditText().setText("");
+            sys.setText("");
+            dias.setText("");
+            cmnt.setText("");
+            hr.setText("");
             Intent intent=new Intent(getApplicationContext(),fetchdata.class);
             startActivity(intent);
             finish();
